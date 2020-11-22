4d0592c534795c8cc980f6e324dd359ce1f60b73
s v <chofexx@gmail.com>
2020-11-22 07:55:34 +0200

    Connecting to View

--- src/ConferenceApp/ConferenceApp/Services/ConferenceService.cs
+++ src/ConferenceApp/ConferenceApp/Services/ConferenceService.cs
@@ -1,4 +1,5 @@
 ﻿using ConferenceApp.Models;
+using System;
 using System.Collections.Generic;
 
 namespace ConferenceApp.Services
@@ -7,7 +8,26 @@ namespace ConferenceApp.Services
     {
         public IList<Conference> GetAll()
         {
-            return new List<Conference>();
+            return new List<Conference>() 
+            {
+                new Conference(){ Name="Conference O1", Location ="Bulgaria, Sofia", Date = DateTime.Now, TicketCost = 15 },
+                new Conference(){ Name="Conference O2", Location ="Bulgaria, Plovdiv", Date = DateTime.Now, TicketCost = 24},
+                new Conference(){ Name="Conference O3", Location ="Bulgaria, Varna", Date = DateTime.Now, TicketCost = 23 },
+                new Conference(){ Name="Conference O4", Location ="Bulgaria, Sofia", Date = DateTime.Now, TicketCost = 34 },
+                new Conference(){ Name="Conference O5", Location ="Bulgaria, Sofia", Date = DateTime.Now, TicketCost = 34 },
+                new Conference(){ Name="Conference O6", Location ="Bulgaria, Plovdiv", Date = DateTime.Now, TicketCost = 67 },
+                new Conference(){ Name="Conference O7", Location ="Bulgaria, Sofia", Date = DateTime.Now, TicketCost = 11 },
+                new Conference(){ Name="Conference O8", Location ="Bulgaria, Sofia", Date = DateTime.Now, TicketCost = 13 },
+                new Conference(){ Name="Conference O9", Location ="Bulgaria, Plovdiv", Date = DateTime.Now, TicketCost = 56 },
+                new Conference(){ Name="Conference OA", Location ="Bulgaria, Varna", Date = DateTime.Now, TicketCost = 12 },
+                new Conference(){ Name="Conference OB", Location ="Bulgaria, Sofia", Date = DateTime.Now, TicketCost = 34 },
+                new Conference(){ Name="Conference OC", Location ="Bulgaria, Sofia", Date = DateTime.Now, TicketCost = 99 },
+                new Conference(){ Name="Conference OD", Location ="Bulgaria, Plovdiv", Date = DateTime.Now, TicketCost = 15 },
+                new Conference(){ Name="Conference OE", Location ="Bulgaria, Varna", Date = DateTime.Now, TicketCost = 23 },
+                new Conference(){ Name="Conference OF", Location ="Bulgaria, Sofia", Date = DateTime.Now, TicketCost = 13 },
+                new Conference(){ Name="Conference OG", Location ="Bulgaria, Plovdiv", Date = DateTime.Now, TicketCost = 89 },
+                new Conference(){ Name="Conference OH", Location ="Bulgaria, Sofia", Date = DateTime.Now, TicketCost = 12 },
+            };
         }
 
     }
--- src/ConferenceApp/ConferenceApp/ViewModels/ConferenceOverviewViewModel.cs
+++ src/ConferenceApp/ConferenceApp/ViewModels/ConferenceOverviewViewModel.cs
@@ -1,24 +1,38 @@
-﻿using ConferenceApp.Services;
+﻿using ConferenceApp.Models;
+using ConferenceApp.Services;
+using System.Collections.ObjectModel;
 
 namespace ConferenceApp.ViewModels
 {
     public class ConferenceOverviewViewModel : BaseViewModel
     {
         private readonly IConferenceService _conferenceService;
+        private ObservableCollection<Conference> _conferences;
 
         public ConferenceOverviewViewModel(IConferenceService conferenceService)
         {
             Title = "Conference Overview";
             _conferenceService = conferenceService;
 
+            Conferences = new ObservableCollection<Conference>();
+
+            Initialize();
         }
 
         public void Initialize()
         {
+            var conferencesServiceCall = _conferenceService.GetAll();
 
+            Conferences = new ObservableCollection<Conference>(conferencesServiceCall);
         }
 
        
+        public ObservableCollection<Conference> Conferences
+        {
+            get { return _conferences; }
+            set { _conferences = value; }
+        }
+
 
     }
 }
